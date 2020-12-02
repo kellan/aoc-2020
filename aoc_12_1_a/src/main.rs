97426b5155fd2c9fc9e7832fa4a9c88a2c895765
Kellan Elliott-McCrea <kelliott@dropbox.com>
2020-12-01 23:23:19 -0500

    not even embarrassed by this solution

--- aoc_12_1_a/src/main.rs
+++ aoc_12_1_a/src/main.rs
@@ -16,21 +16,42 @@ fn main() {
         })
         .collect();
 
-    match pair(numbers, target) {
+    match pair(&numbers, target) {
         Some((a,b)) => {
             println!("{} * {} = {}", a, b, a*b);
         }
         None => println!("no match"),
     }
-//    triple(numbers, target);
+
+    match triple(&numbers, target) {
+        Some((a,b,c)) => {
+            println!("{} * {} * {} = {}", a, b, c, a*b*c);
+        }
+        None => println!("no match"),
+    }
 
 }
     	
-fn pair(numbers: HashSet<i32>, target: i32) -> Option<(i32, i32)> {
-    for n in &numbers {
+fn pair(numbers: &HashSet<i32>, target: i32) -> Option<(i32, i32)> {
+    for n in numbers {
         let query = target-n;
         if numbers.contains(&query) {
             return Some((*n, target-n));
+        }
+    }
+
+    None
+}
+
+fn triple(numbers: &HashSet<i32>, target: i32) -> Option<(i32, i32, i32)> {
+    for n in numbers {
+        let rem = target - n;
+        let result  = pair(numbers, rem);
+        match result {
+            Some((a,b)) => {
+                return Some((a,b,*n));
+            }
+            None => continue
         }
     }
 
