c0aff897c58b045c019b03fc8fff95a1a48b7a50
Kellan Elliott-McCrea <kelliott@dropbox.com>
2020-12-04 09:23:13 -0500

    kiss

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,20 +1,59 @@
 use std::fs;
 
 fn main() {
-    
-    let mut right: usize = 0;
-    let mut tree_count = 0;
-    let slope: usize = 3;
 
-    let input = fs::read_to_string("sample_input.txt")
+    let input = fs::read_to_string("input.txt")
     	.expect("Something went wrong reading the file");
 
+    let part1 = part1(&input);
+    let part2 = part2(&input);
+
+    println!("Trees part1: {} part2: {}", part1, part2);
+ }
+
+ fn part1(input: &String) -> usize {
+
+    let tree_count = toboggan(input, 3, 1);
+    return tree_count;
+ }
+
+ fn part2(input: &String) -> usize {
+ 	let slopes = [(1,1), (3,1), (5,1), (7,1), (1,2)];
+ 	//let mut counts: Vec<usize> = [];
+
+ 	let mut count = 1;
+
+ 	for slope in slopes.iter() {
+	 	let found = toboggan(input, slope.0, slope.1);
+	 	count = found * count;
+	}
+
+    return count;	
+ }
+
+ // fn part2() -> usize {
+ // 	let input = fs::read_to_string("input.txt")
+ //    	.expect("Something went wrong reading the file");
+
+ // 	let slopes = [(1,1), (3,1), (5,1), (7,1), (1,2)];
     
-    let mut lines = input.lines();
+ //    for slope in slopes.iter() {
+ //    	let tree_count = toboggan(input, 1, 2);
+ //    }
+
+ //    return 0
+ // }
+
+fn toboggan(input: &String, slope_right: usize, slope_down: usize) -> usize {
+
+	let mut right: usize = 0;
+    let mut tree_count = 0;
+    
+    let mut lines = input.lines().step_by(slope_down);
 
     // consume first line
     let size = lines.next().unwrap().chars().count();
-    right = right + slope;
+    right = right + slope_right;
 
     for line in lines {
     	
@@ -22,8 +61,8 @@ fn main() {
     		tree_count += 1;
     	}
 
-    	right = (right + slope) % size;
+    	right = (right + slope_right) % size;
     }
 
-    println!("Trees: {}", tree_count);
- }
+    return tree_count;
+}
