7a39dc62ba3e58e2f462cc955fb221573b67bb3d
Kellan Elliott-McCrea <kelliott@dropbox.com>
2020-12-04 11:17:21 -0500

    first pass, off by one

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,3 +1,40 @@
+use std::fs;
+
 fn main() {
     println!("Hello, world!");
+
+    let input = fs::read_to_string("input.txt")
+    	.expect("Something went wrong reading the file");
+
+
+    	let mut passport_fields: Vec<&str> = Vec::new();
+    	let mut valid_passports = 0;
+
+    	for line in input.lines() {
+    		println!("{}", line);
+
+    		// blank line
+    		if line.is_empty() {
+    			if passport_fields.len() >= 8 {
+    				valid_passports +=1;
+    			} else if passport_fields.len() >= 7 {
+    				if ! passport_fields.contains(&"cid") {
+    					valid_passports += 1;
+    				}
+    			}
+    			println!("{}", valid_passports);
+
+    			passport_fields = Vec::new();
+    			continue;
+    		}
+
+    		for field in line.split_ascii_whitespace() {
+    			let field: Vec<&str> = field.split(':').collect();
+    			passport_fields.push(field[0]);
+    		}
+
+    		
+    	}
+
+    	//println!("valid_passports {}", valid_passports);
 }
