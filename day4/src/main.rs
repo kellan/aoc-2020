0e39928d22cb946ffe9bc1b824f11f7fe625b626
Kellan Elliott-McCrea <kelliott@dropbox.com>
2020-12-04 12:32:04 -0500

    working part 1

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -10,31 +10,37 @@ fn main() {
     	let mut passport_fields: Vec<&str> = Vec::new();
     	let mut valid_passports = 0;
 
-    	for line in input.lines() {
-    		println!("{}", line);
+    	let mut lines = input.lines();
 
-    		// blank line
-    		if line.is_empty() {
-    			if passport_fields.len() >= 8 {
-    				valid_passports +=1;
-    			} else if passport_fields.len() >= 7 {
-    				if ! passport_fields.contains(&"cid") {
-    					valid_passports += 1;
-    				}
+    	loop {
+    		let line = lines.next();
+
+    		if line.is_some() && !line.unwrap().is_empty() {
+    			
+    			for field in line.unwrap().split_ascii_whitespace() {
+    				let field: Vec<&str> = field.split(':').collect();
+    				passport_fields.push(field[0]);
     			}
-    			println!("{}", valid_passports);
 
-    			passport_fields = Vec::new();
     			continue;
     		}
 
-    		for field in line.split_ascii_whitespace() {
-    			let field: Vec<&str> = field.split(':').collect();
-    			passport_fields.push(field[0]);
+    		// if we got here it's because we've reached the end of a passport
+    		
+    		if passport_fields.len() >= 8 {
+    			valid_passports +=1;
+    		} else if passport_fields.len() >= 7 {
+    			if ! passport_fields.contains(&"cid") {
+    				valid_passports += 1;
+    			}
     		}
 
-    		
+    		passport_fields = Vec::new();
+
+    		if line.is_none() {
+    			break;
+    		}
     	}
 
-    	//println!("valid_passports {}", valid_passports);
+    	println!("valid_passports {}", valid_passports);
 }
