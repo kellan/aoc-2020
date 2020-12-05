adc407437d39a763124251b9753675bdae627518
Kellan Elliott-McCrea <kelliott@dropbox.com>
2020-12-05 09:02:19 -0500

    binary

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -5,25 +5,25 @@ fn main() {
     println!("Hello, world!");
 
     let input = fs::read_to_string("input.txt")
-    	.expect("Something went wrong reading the file");
+        .expect("Something went wrong reading the file");
 
     let mut largest_seat_id = 0;
     let mut seat_ids: Vec<usize> = Vec::new();
 
     for line in input.lines() {
-    	let seat_id = seat_id(line);
+        let seat_id = seat_id(line);
         seat_ids.push(seat_id);
 
-    	if seat_id > largest_seat_id {
-    		largest_seat_id = seat_id
-    	}
+        if seat_id > largest_seat_id {
+            largest_seat_id = seat_id
+        }
     }
 
     println!("largest_seat_id {}", largest_seat_id);
 
-	seat_ids.sort();
+    seat_ids.sort();
 
-	for i in 1..seat_ids.len() {
+    for i in 1..seat_ids.len() {
         //println!("{} {} {}", i, seat_ids[i], seat_ids[i-1]);
         if seat_ids[i] != seat_ids[i-1]+1 {
             println!("part2 {}", seat_ids[i]-1);
@@ -32,7 +32,7 @@ fn main() {
 }
 
 fn seat_id(seat_rule: &str) -> usize {
-	let bin_seat = seat_rule.replace("F", "0").replace("B", "1")
+    let bin_seat = seat_rule.replace("F", "0").replace("B", "1")
         .replace("L", "0").replace("R", "1");
 
     return usize::from_str_radix(&bin_seat, 2).unwrap();
