f36bc773162d2f0832abd689d4a18956edd6c7c3
Kellan Elliott-McCrea <kelliott@dropbox.com>
2020-12-05 08:03:38 -0500

    part 1

---
+++ day5/src/main.rs
@@ -0,0 +1,53 @@
+use std::fs;
+
+
+fn main() {
+    println!("Hello, world!");
+
+    let input = fs::read_to_string("input.txt")
+    	.expect("Something went wrong reading the file");
+
+    let mut largest_seat_id = 0;
+
+    for line in input.lines() {
+    	let seat_id = seat_id(line);
+    	if seat_id > largest_seat_id {
+    		largest_seat_id = seat_id
+    	}
+    }
+
+    println!("largest {}", largest_seat_id);
+    
+}
+
+fn seat_id(seat_rule: &str) -> usize {
+	let mut row = [0, 127];
+
+    for c in seat_rule[0..7].chars() {
+    	let diff = (row[1] - row[0])/2 as usize;
+    	
+    	if c == 'F' {
+    		row = [row[0], row[0]+diff];
+    	} else if c == 'B' {
+    		row = [row[0]+diff+1, row[1]];
+    	}
+    }
+
+    let mut col = [0, 7];
+
+    for c in seat_rule[7..10].chars() {
+    	let diff = (col[1] - col[0])/2 as usize;
+
+    	if c == 'L' {
+    		col = [col[0], col[0]+diff];
+    	} else if c == 'R' {
+    		col = [col[0]+diff+1, col[1]];
+    	}
+    }
+
+    let seat_id = row[0]*8 + col[0];
+
+    //println!("{:?}, {:?} = {}", row, col, seat_id);
+
+    return seat_id;
+}
